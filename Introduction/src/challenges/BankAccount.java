26106d32747c309eaec795eafa0582a0829cf129
Butch Bet <butchbet01@gmail.com>
2023-04-11 13:39:31 -0500

    Created the BanckAccount class.

---
+++ Introduction/src/challenges/BankAccount.java
@@ -0,0 +1,25 @@
+package challenges;
+
+public class BankAccount {
+    public String name;
+    public Double balance;
+    
+    public BankAccount(){}
+    
+    public BankAccount(String name) {
+        this.name = name;
+    }
+    
+    public BankAccount(String name, Double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+    
+    public void deposit(Double money) {
+        this.balance += money;
+    }
+    
+    public void withdraw(Double money) {
+        this.balance -= money;
+    }
+}
