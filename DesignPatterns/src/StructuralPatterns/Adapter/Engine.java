6f3d21bad6231e7649726109fc30a03add90b488
Butch Bet <butchbet01@gmail.com>
2023-05-03 21:00:12 -0500

    Created the Engine abstract class and the commonEngine class.

---
+++ DesignPatterns/src/StructuralPatterns/Adapter/Engine.java
@@ -0,0 +1,7 @@
+package StructuralPatterns.Adapter;
+
+public abstract class Engine {
+    public abstract void turnOn();
+    public abstract void speedUp();
+    public abstract void turnOff();
+}
---
+++ DesignPatterns/src/StructuralPatterns/Adapter/commonEngine.java
@@ -0,0 +1,23 @@
+package StructuralPatterns.Adapter;
+
+public class commonEngine extends Engine{
+    public commonEngine() {
+        super();
+        System.out.println("Creating common engine.");
+    }
+    
+    @Override
+    public void turnOn() {
+        System.out.println("Turning on common engine.");
+    }
+    
+    @Override
+    public void speedUp() {
+        System.out.println("Speeding up common engine.");
+    }
+    
+    @Override
+    public void turnOff() {
+        System.out.println("Turning off common engine.");
+    }
+}
