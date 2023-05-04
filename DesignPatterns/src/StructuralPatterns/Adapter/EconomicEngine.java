36ac05cd82c116dbe5f052e5c10bbd6e1dee4522
Butch Bet <butchbet01@gmail.com>
2023-05-03 21:03:33 -0500

    Created the EconomicEngine class.

---
+++ DesignPatterns/src/StructuralPatterns/Adapter/EconomicEngine.java
@@ -0,0 +1,23 @@
+package StructuralPatterns.Adapter;
+
+public class EconomicEngine extends Engine {
+    public EconomicEngine() {
+        super();
+        System.out.println("Creating economic engine.");
+    }
+    
+    @Override 
+    public void turnOn() {
+        System.out.println("Turning on economic engine.");
+    }
+    
+    @Override
+    public void speedUp() {
+        System.out.println("Speeding up economic engine.");
+    }
+    
+    @Override 
+    public void turnOff() {
+        System.out.println("Turning off economic engine.");
+    }
+}
