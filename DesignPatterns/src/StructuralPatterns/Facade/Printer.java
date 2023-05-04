e1ed7454f8092b80350ac5dda27439ba703d4b16
Butch Bet <butchbet01@gmail.com>
2023-05-04 17:58:47 -0500

    Created the Printer class in the Facade package.

---
+++ DesignPatterns/src/StructuralPatterns/Facade/Printer.java
@@ -0,0 +1,44 @@
+package StructuralPatterns.Facade;
+
+public class Printer {
+    private String documentType;
+    private String sheet;
+    private boolean color;
+    private String text;
+
+    public String getDocumentType() {
+        return documentType;
+    }
+
+    public void setDocumentType(String documentType) {
+        this.documentType = documentType;
+    }
+
+    public String getSheet() {
+        return sheet;
+    }
+
+    public void setSheet(String sheet) {
+        this.sheet = sheet;
+    }
+
+    public boolean isColor() {
+        return color;
+    }
+
+    public void setColor(boolean color) {
+        this.color = color;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+    public void print() {
+        System.out.println("Printing: " + text + ", type: " + documentType + ", color: " + color + ", sheet: " + sheet);
+    }
+}
