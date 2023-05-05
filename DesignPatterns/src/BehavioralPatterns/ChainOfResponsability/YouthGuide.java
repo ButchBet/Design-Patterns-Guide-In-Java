5d19ba2cfe0d6ec3aa9a4c4c2bd016a166ea516d
Butch Bet <butchbet01@gmail.com>
2023-05-05 13:20:10 -0500

    Created the YouthGuide class implementing the GuideSelector interface.

---
+++ DesignPatterns/src/BehavioralPatterns/ChainOfResponsability/YouthGuide.java
@@ -0,0 +1,20 @@
+package BehavioralPatterns.ChainOfResponsability;
+
+public class YouthGuide implements GuideSelector {
+    private GuideSelector next;
+    
+    @Override
+    public GuideSelector getNext() {
+        return next;
+    }
+    
+    @Override
+    public void setNext(GuideSelector next) {
+        this.next = next;
+    }
+    
+    @Override
+    public void assignGuide(GuideSelector guide) {
+        this.next.setNext(guide);
+    }
+}
