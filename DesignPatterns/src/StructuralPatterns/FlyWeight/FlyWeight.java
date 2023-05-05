130ba4d9e8c95afafd13f2ee0835d44be352e4c0
Butch Bet <butchbet01@gmail.com>
2023-05-05 11:29:45 -0500

    Finished the FlyWeightFactory class and imlemented in the FlyWeight class, by now the Structural patterns are finished, we just need to make the practice.

--- DesignPatterns/src/StructuralPatterns/FlyWeight/FlyWeight.java
+++ DesignPatterns/src/StructuralPatterns/FlyWeight/FlyWeight.java
@@ -4,6 +4,36 @@ public class FlyWeight {
     public FlyWeight() {}
     
     public void main() {
-        System.out.println("FlyWeight.");
-    }
+        FlyWeight fw = new FlyWeight();
+        
+        String position[] = {
+            "Bilingual Agent",
+            "Management student",
+            "Medicine student",
+            "Market store manager",
+            "Sales assitant",
+            "High school student",
+            "Bilingual Agent",
+            "I don't know what manaeger"
+        };        
+        
+        String names[] = {
+            "Kevin",
+            "Kellys",
+            "Karem",
+            "Andrea",
+            "Diana",
+            "Taliana",
+            "Dayana",
+            "Luisa"
+        };
+        
+        double salaries[] = {
+            2596000,
+            300000,
+            300000,
+            30000000,
+            
+        };
+        }
 }
--- DesignPatterns/src/StructuralPatterns/FlyWeight/FlyWeightFactory.java
+++ DesignPatterns/src/StructuralPatterns/FlyWeight/FlyWeightFactory.java
@@ -3,9 +3,9 @@ package StructuralPatterns.FlyWeight;
 public class FlyWeightFactory {
     Employee employee;
     
-    public void listEmployees(String position[], String name[], double salary[]) {
-        for(int i = 0; i < position.length; i++) {
-            employee = new Employee(position[i], name[i], salary[i]);
+    public void listEmployees(String positions[], String names[], double salaries[]) {
+        for(int i = 0; i < positions.length; i++) {
+            employee = new Employee(positions[i], names[i], salaries[i]);
             
             System.out.println("Employee's information:\n" + employee.information() + "\n");
         }
