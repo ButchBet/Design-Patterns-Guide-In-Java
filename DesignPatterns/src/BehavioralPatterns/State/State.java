08252df5947b38ef1a270f91b916f6ecdebccea8
Butch Bet <butchbet01@gmail.com>
2023-05-07 22:05:11 -0500

    Implemented the Phone class in the State class in order to test.

--- DesignPatterns/src/BehavioralPatterns/State/State.java
+++ DesignPatterns/src/BehavioralPatterns/State/State.java
@@ -4,6 +4,18 @@ public class State {
     public State() {}
     
     public void main() {
-        System.out.println("State");
+        Phone phone = new Phone("123");
+        
+        phone.activate();
+        phone.state();
+        phone.deactivate();
+        phone.state();
+        
+        phone = new Phone("321");
+        
+        phone.activate();
+        phone.state();
+        phone.deactivate();
+        phone.state();
     }
 }
